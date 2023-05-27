5cd46e6b47de1a4f7b972efca9bacf26dae480de
Armando Souza <armandosouzafalzi@gmail.com>
2023-05-27 01:45:58 -0300

    exercise 1.11 added

--- part1/src/App.js
+++ part1/src/App.js
@@ -8,7 +8,10 @@ const Button = ({onClick, value}) => {
 
 const StatisticLine = ({text, value}) => {
   return (
-    <p>{text} {value}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -22,12 +25,16 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <>
       <h1>statistics</h1>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={good + neutral + bad} />
-      <StatisticLine text="average" value={(good * 1 + bad * -1) / (good + neutral + bad)} />
-      <StatisticLine text="positive" value={`${(good / (good + neutral + bad)) * 100} %`} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={good + neutral + bad} />
+          <StatisticLine text="average" value={(good * 1 + bad * -1) / (good + neutral + bad)} />
+          <StatisticLine text="positive" value={`${(good / (good + neutral + bad)) * 100} %`} />
+        </tbody>
+      </table>
     </>
   )
 }
